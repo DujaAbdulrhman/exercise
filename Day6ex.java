bcce2318b5b7b1a5090f69f0e6c1691b27454316
Duja Abdurhman <134400247+DujaAbdulrhman@users.noreply.github.com>
2025-02-17 23:22:50 +0300

    Update Day6ex.java

--- Day6ex.java
+++ Day6ex.java
@@ -12,7 +12,7 @@ public class Day6ex {
         Sample Output: false */
 
        /*int [] num={50,-20,0,30,40,60,10};
-       if (num[0]==num[6]){
+       if (num[0]==nnum[num.length - 1]){
            System.out.println(true);
        }else System.out.println("false");*/
 
@@ -33,17 +33,18 @@ public class Day6ex {
         int sum=0;
         int avarege=0;
         int grater = 0;
-        for (int i = 0; i <num.length ; i++) {
-            sum=num[i];
+        ArrayList<Integer> newlisr=new ArrayList<>();
+        for (int j : num) {
+            sum += j;
         }
         avarege = sum/num.length;
-        for (int i = 0; i <num.length ; i++) {
-            if (num[i]>avarege){
-                grater=num[i];
-            }
+        for (int j : num) {
+            if (j > avarege) {
+                newlisr.add(j);
+            } else continue;
         }
         System.out.println("The average of the said array is: "+avarege+" The numbers in the said array that are\n" +
-                "greater than the average are: "+grater )*/;
+                "greater than the average are: "+newlisr);*/
 
 
         /*3.Write a Java program to get the larger value between first and last
@@ -53,12 +54,21 @@ public class Day6ex {
         Sample Output:
         Larger value between first and last element: 40*/
 
-       /* int [] larger={20,30,40};
-        if(larger[0]>larger[1]&& larger[0]>larger[2]){
-            System.out.println("Larger value between first and last element:"+larger[0]);
-        } else if (larger[1]>larger[2]) {
-            System.out.println("Larger value between first and last element:"+larger[1]);
-        } else System.out.println("Larger value between first and last element: "+larger[2]);
+       /*  int[] larger = {20, 30, 40};
+        int n = larger.length;
+
+        if (n > 0) {
+
+            int largestValue = larger[0];
+
+            for (int i = 1; i < n; i++) {
+                if (larger[i] > largestValue) {
+                    largestValue = larger[i];
+                }
+            }
+            System.out.println("Original Array: " + java.util.Arrays.toString(larger));
+            System.out.println("Largest value in the array: " + largestValue);
+        }
 */
 
         /*4.Write a Java program to swap the first and last elements of an array and
@@ -117,10 +127,18 @@ public class Day6ex {
         [2,3,6,6,4] [2,3,6,6,4]
         Sample Output: true */
 
-        /*int[] num1={2,3,6,6,4};
-        int []num2={2,3,6,6,4};
-
-        System.out.println(Arrays.equals(num1, num2));*/
+       /* int[] num1={2,3,6,6,4};
+        int []num2={2,3,6,6,4,};
+        boolean areEqual = true;
+        
+        if (num1.length != num2.length) {
+            areEqual = false;
+        } else {
+            for (int i = 0; i < num1.length; i++) {
+                if (num1[i] == num2[i]) {
+                    areEqual = true;
+                }else {areEqual=false;}
+            }}System.out.println(areEqual);*/
 
 
 
